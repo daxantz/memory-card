402918ff2406ae50b0e8dc79b9272deb5bee070f
Dax Davis <daxantz@gmail.com>
2024-07-30 21:02:27 -0400

    main funcitonality done

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,30 +3,6 @@ import md5 from "crypto-js/md5";
 import { publicKey, privateKey } from "./config";
 
 export default function Card({ name, imageUrl, selectCard }) {
-  // const [imageUrl, setImageUrl] = useState('');
-  // const [name, setName] = useState('');
-  // async function fetchData(){
-  //     try{
-  //         const ts = Date.now().toString();
-  //         const hash = md5(ts + privateKey + publicKey);
-  //         const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${characterName}&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-  //         const data = await response.json()
-  //         const image = data.data.results[0].thumbnail.path + '.jpg';
-  //         const name = data.data.results[0].name;
-  //         setImageUrl(image)
-  //         setName(name)
-  //         console.log(data)
-
-  //     } catch(err){
-  //         console.log(err);
-  //     }
-  // }
-
-  // useEffect(()=>{
-
-  //     fetchData();
-  // }, [])
-
   return (
     <div
       onClick={(e) => selectCard(e)}
---
+++ src/allCharacters.js
@@ -0,0 +1,16 @@
+const allCharacters = [
+  "storm",
+  "beast",
+  "black panther",
+  "iron man",
+  "deadpool",
+  "cyclops",
+  "captain america",
+  "groot",
+  "blade",
+  "gambit",
+  "wolverine",
+  "punisher",
+];
+
+export default allCharacters;
